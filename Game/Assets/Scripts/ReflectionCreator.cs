a499899d8138de40069ad6fe462d0cccbc9bb349
yili0828 <yili0828@gmail.com>
2018-11-02 14:21:45 -0400

    Update - ReflectionCreator

---
+++ Game/Assets/Scripts/ReflectionCreator.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ReflectionCreator : MonoBehaviour {
+
+    public GameObject reflection;
+    public Transform spawnPoint;
+    GameObject currentObject;
+
+    // Use this for initialization
+    void Start () {
+        currentObject = Instantiate(reflection, spawnPoint.position, spawnPoint.rotation);
+    }
+	
+}
