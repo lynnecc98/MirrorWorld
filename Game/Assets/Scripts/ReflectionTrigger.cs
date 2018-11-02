d47cb145d224ede3de1d85c9b2126d4118849de4
Johnnyhoboy <johnhallho@gmail.com>
2018-11-02 01:16:33 -0400

    Added win/lose scenes

---
+++ Game/Assets/Scripts/ReflectionTrigger.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ReflectionTrigger : MonoBehaviour {
+    public bool triggered = false;
+    private bool inTrigger = false;
+    elementType myElement;
+    GameObject player;
+
+    private void Start()
+    {
+        myElement = GetComponent<elementType>();
+        player = GameObject.Find("PlayableCharacter");
+    }
+
+    private void Update()
+    {
+        if (inTrigger)
+        {
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                Debug.Log("triggered reflection");
+                triggered = !triggered;
+
+                if (myElement.element == player.GetComponent<elementType>().element)
+                {
+                    SceneManager.LoadScene("WinScene");
+                }
+                else
+                {
+                    SceneManager.LoadScene("LoseScene");
+                }
+
+            }
+        }
+    }
+
+    //private void OnTriggerStay(Collider player)
+    //{
+    //    Debug.Log("inside");
+    //    if (player.attachedRigidbody)
+    //    {
+
+    //    }
+    //}
+    private void OnTriggerEnter(Collider other)
+    {
+        inTrigger = true;
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        inTrigger = false;
+    }
+}
