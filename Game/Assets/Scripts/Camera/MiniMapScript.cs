df60ce704a454054dbd1e6fc72a95b2fa4829243
Johnnyhoboy <johnhallho@gmail.com>
2018-11-18 19:32:15 -0500

    Added Minimap

---
+++ Game/Assets/Scripts/Camera/MiniMapScript.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MiniMapScript : MonoBehaviour {
+
+    public Transform player;
+
+    void LateUpdate()
+    {
+        Vector3 newPosition = player.position;
+        newPosition.y = transform.position.y;
+        transform.position = newPosition;
+    }
+}
