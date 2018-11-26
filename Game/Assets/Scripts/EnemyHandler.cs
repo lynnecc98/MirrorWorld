166f325e0971626f76880a3563ed918ef67c144a
yili0828 <yili0828@gmail.com>
2018-11-25 22:05:28 -0500

    Enemy Handler
    generate enemy based on the element type of the room.

---
+++ Game/Assets/Scripts/EnemyHandler.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyHandler : MonoBehaviour
+{
+
+    public GameObject enemy;
+    public int count;
+    public Transform[] spawnPoints;
+    public GameObject[] enemys;
+    public Material redM, greenM, blueM;
+
+
+    private void Start()
+    {
+        elementType curElement = GetComponent<elementType>();
+        enemys = new GameObject[count];
+        for (int i = 0; i < count; i++)
+        {
+            enemys[i] =  Instantiate(enemy, spawnPoints[i].position, spawnPoints[i].rotation);
+            elementType element = enemys[i].GetComponent<elementType>();
+            element.element = curElement.element;
+
+        }
+    }
+
+}
---
+++ Game/Assets/Scripts/enemyType.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class enemyType : MonoBehaviour {
+
+    public Material redM, greenM, blueM;
+
+    // Use this for initialization
+    void Start () {
+        elementType element = GetComponent<elementType>();
+        if (element.element == Element.Red)
+        {
+            GetComponent<MeshRenderer>().material = redM;
+        }
+        if (element.element == Element.Blue)
+        {
+            GetComponent<MeshRenderer>().material = blueM;
+        }
+        if (element.element == Element.Green)
+        {
+            GetComponent<MeshRenderer>().material = greenM;
+        }
+
+    }
+	
+    
+}
