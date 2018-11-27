2bf1d29ccf13c987d891fce9822b8e4b46e9d2eb
yili0828 <yili0828@gmail.com>
2018-11-26 22:12:27 -0500

    Interactivable bridges
    I implement the interactivable bridges in the alpha demo copy.

--- Game/Assets/Scripts/EnvironmentSetUp.cs
+++ Game/Assets/Scripts/EnvironmentSetUp.cs
@@ -10,6 +10,7 @@ enum roomType
 public class EnvironmentSetUp : MonoBehaviour {
 
     public GameObject[] floor;
+    public GameObject[] BridgeL;
     public GameObject EnemyRoom;
     public GameObject TriggerRoom;
     public GameObject HealthRoom;
@@ -17,6 +18,8 @@ public class EnvironmentSetUp : MonoBehaviour {
     public GameObject endRoom;
     public GameObject keyRoom;
     public GameObject bossRoom;
+    public GameObject bridge;
+    public GameObject trig;
     public GameObject playr;
 
     List <roomType> rooms = new List<roomType>();
@@ -30,9 +33,21 @@ public class EnvironmentSetUp : MonoBehaviour {
     roomType goal = roomType.goalRoom;
     roomType boss = roomType.bossRoom;
 
+    int[] bridgeMap1 = {1, 1, 1, 1, 0, 1, 1, 0, 1, 1, 1, 1, 1, 0, 1, 0, 1, 0, 0, 1, 0, 1, 0, 1};
+    int[] bridgeMap2 = {1, 0, 1, 0, 1, 0, 0, 1, 0, 1, 0, 1, 1, 1, 1, 1, 0, 1, 1, 0, 1, 1, 1, 1};
+
+    List<GameObject> curBridges;
+
+    bool trigg = false;
+
+    GameObject curTrig;
+
+
+
 
     // Use this for initialization
     void Start () {
+        curBridges = new List<GameObject>();
 		for(int i = 0; i< 3; i++)
         {
             rooms.Add(enemy1);
@@ -99,10 +114,8 @@ public class EnvironmentSetUp : MonoBehaviour {
             if (rooms[i] == roomType.playerRoom)
             {
                 Instantiate(StartingRoom, floor[i].transform.position, floor[i].transform.rotation);
-                if (rooms[i] == roomType.playerRoom)
-                {
-                    playr.transform.position = floor[i].transform.position;
-                }
+                playr.transform.position = floor[i].transform.position;
+                curTrig = Instantiate(trig, floor[i].transform.position, floor[i].transform.rotation);
             }
             if (rooms[i] == roomType.triggerRoom)
             {
@@ -112,15 +125,61 @@ public class EnvironmentSetUp : MonoBehaviour {
             {
                 Instantiate(keyRoom, floor[i].transform.position, floor[i].transform.rotation);
             }
+        }
+
+        for (int i = 0; i < BridgeL.Length; i++)
+        {
+            if (bridgeMap1[i] == 1)
+            {
+                GameObject temp = Instantiate(bridge, BridgeL[i].transform.position, BridgeL[i].transform.rotation);
+                curBridges.Add(temp);
+            }
+        }
+
+    }
 
 
+    private void Update()
+    {
+        HallwayTrigger hallway = curTrig.GetComponent<HallwayTrigger>();
+        if (trigg == false && hallway.triggered == true)
+        {
+            for(int i = 0; i < curBridges.Count; i++)
+            {
+                Destroy(curBridges[i]); 
+            }
+            curBridges.Clear();
+            for(int i = 0; i < bridgeMap2.Length; i++)
+            {
+                if (bridgeMap2[i] == 1)
+                {
+                    GameObject temp = Instantiate(bridge, BridgeL[i].transform.position, BridgeL[i].transform.rotation);
+                    curBridges.Add(temp);
+                }
+            }
+            trigg = hallway.triggered;
+        }
+
+        if (trigg == true && hallway.triggered == false)
+        {
+            for (int i = 0; i < curBridges.Count; i++)
+            {
+                Destroy(curBridges[i]);
+            }
+            curBridges.Clear();
+            for (int i = 0; i < bridgeMap1.Length; i++)
+            {
+                if (bridgeMap1[i] == 1)
+                {
+                    GameObject temp = Instantiate(bridge, BridgeL[i].transform.position, BridgeL[i].transform.rotation);
+                    curBridges.Add(temp);
+                }
+            }
+            trigg = hallway.triggered;
         }
 
         
-        
-
-
 
     }
-	
+
 }
