8268c45376aff62ec99572a595f6d0864b897699
yili0828 <yili0828@gmail.com>
2018-11-26 19:07:24 -0500

    Rooms

--- Game/Assets/Scripts/EnvironmentSetUp.cs
+++ Game/Assets/Scripts/EnvironmentSetUp.cs
@@ -13,6 +13,10 @@ public class EnvironmentSetUp : MonoBehaviour {
     public GameObject EnemyRoom;
     public GameObject TriggerRoom;
     public GameObject HealthRoom;
+    public GameObject StartingRoom;
+    public GameObject endRoom;
+    public GameObject keyRoom;
+    public GameObject bossRoom;
     public GameObject playr;
 
     List <roomType> rooms = new List<roomType>();
@@ -63,7 +67,7 @@ public class EnvironmentSetUp : MonoBehaviour {
 
         for(int i = 0; i < rooms.Count; i++)
         {
-            if(rooms[i] == roomType.enemyRoomR || rooms[i] == roomType.enemyRoomB || rooms[i] == roomType.enemyRoomG || rooms[i] == roomType.bossRoom) 
+            if(rooms[i] == roomType.enemyRoomR || rooms[i] == roomType.enemyRoomB || rooms[i] == roomType.enemyRoomG) 
             {
                 GameObject temp =  Instantiate(EnemyRoom, floor[i].transform.position, floor[i].transform.rotation);
                 elementType tempElement = temp.GetComponent<elementType>();
@@ -80,17 +84,33 @@ public class EnvironmentSetUp : MonoBehaviour {
                     tempElement.element = Element.Green;
                 }
             }
-            if (rooms[i] == roomType.playerRoom || rooms[i] == roomType.healthRoom || rooms[i] == roomType.goalRoom)
+            if(rooms[i] == roomType.bossRoom)
+            {
+                Instantiate(bossRoom, floor[i].transform.position, floor[i].transform.rotation);
+            }
+            if ( rooms[i] == roomType.healthRoom)
             {
                 Instantiate(HealthRoom, floor[i].transform.position, floor[i].transform.rotation);
+            }
+            if (rooms[i] == roomType.goalRoom)
+            {
+                Instantiate(endRoom, floor[i].transform.position, floor[i].transform.rotation);
+            }
+            if (rooms[i] == roomType.playerRoom)
+            {
+                Instantiate(StartingRoom, floor[i].transform.position, floor[i].transform.rotation);
                 if (rooms[i] == roomType.playerRoom)
                 {
                     playr.transform.position = floor[i].transform.position;
                 }
             }
-            if (rooms[i] == roomType.triggerRoom || rooms[i] == roomType.keyRoom)
+            if (rooms[i] == roomType.triggerRoom)
             {
                 Instantiate(TriggerRoom, floor[i].transform.position, floor[i].transform.rotation);
+            }
+            if (rooms[i] == roomType.keyRoom)
+            {
+                Instantiate(keyRoom, floor[i].transform.position, floor[i].transform.rotation);
             }
 
 
