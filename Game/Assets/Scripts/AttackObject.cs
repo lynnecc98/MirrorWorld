8abfcc869c088affa8631b3705e2c4f636e8c302
yili0828 <yili0828@gmail.com>
2018-11-28 16:35:20 -0500

    Rooms fixed

--- Game/Assets/Scripts/AttackObject.cs
+++ Game/Assets/Scripts/AttackObject.cs
@@ -19,7 +19,7 @@ public class AttackObject : MonoBehaviour {
             }
             
         }
-        Destroy(this.gameObject);
+        Destroy(this.gameObject, 0.1f);
     }
 
     void Update()
