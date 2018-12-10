a103519f01390ef2439421a5b519060e572a03cb
juliepierides <juliepierides@gatech.edu>
2018-12-10 18:02:19 -0500

    Added falling sound

--- Game/Assets/Scripts/PlayerHealth.cs
+++ Game/Assets/Scripts/PlayerHealth.cs
@@ -14,6 +14,7 @@ public class PlayerHealth : MonoBehaviour
     public bool damaged;
     public bool isDead = false;
     public Slider healthSlider;
+    public AudioClip falling;
 
 
 
@@ -43,10 +44,17 @@ public class PlayerHealth : MonoBehaviour
             Destroy(gameObject, 2f);
             SceneManager.LoadScene("LoseScene");
         }
-        if (player.transform.position.y < -9)
+        if (player.transform.position.y < 0)
+        {
+            AudioSource fall = GetComponent<AudioSource>();
+            fall.PlayOneShot(falling, 0.1F);
+        }
+
+        if (player.transform.position.y < -20)
         {
             isDead = true;
         }
+        
     }
 
     public void TakeDamage(int amount)
