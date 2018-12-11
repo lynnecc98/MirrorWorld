3416f4110749d5dd3d0a91a20f4226672e8bff5f
lynnecc98 <lynnecc98@gmail.com>
2018-12-11 05:17:16 -0500

    created proximity animation. smoother attack and can attack while moving

---
+++ Game/Assets/Appear.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Appear : MonoBehaviour {
+    private Animator anim;
+	// Use this for initialization
+	void Start () {
+        anim = GetComponentInChildren<Animator>();
+	}
+
+    private void OnTriggerEnter(Collider c)
+    {
+        if (c.attachedRigidbody && c.attachedRigidbody.gameObject == GameObject.FindGameObjectWithTag("Player")) {
+            anim.SetBool("InBoat", true);
+        }
+    }
+    private void OnTriggerExit(Collider c)
+    {
+        if (c.attachedRigidbody && c.attachedRigidbody.gameObject == GameObject.FindGameObjectWithTag("Player"))
+        {
+            anim.SetBool("InBoat", false);
+        }
+    }
+
+}
