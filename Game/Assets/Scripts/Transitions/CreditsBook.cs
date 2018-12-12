99922077ade9bb93f112876f0899b7f48bad2ae4
Johnnyhoboy <johnhallho@gmail.com>
2018-12-12 00:55:23 -0500

    Added credits

---
+++ Game/Assets/Scripts/Transitions/CreditsBook.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CreditsBook : MonoBehaviour {
+
+	public void loadCredits () {
+
+        GameObject.Find("CreditsBook").GetComponent<Image>().enabled = true;
+        bool status = GameObject.Find("CreditsBook").GetComponent<Image>().enabled;
+
+        if (Input.GetKeyDown(KeyCode.Space))
+            GameObject.Find("CreditsBook").GetComponent<Image>().enabled = !status;
+    }
+
+
+}
